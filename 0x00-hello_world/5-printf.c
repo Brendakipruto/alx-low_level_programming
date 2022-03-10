048f1397bccfa31e02fc367cdab3e8fa551f53d5
Brendakipruto <brendakipruto16@gmail.com>
2022-03-10 09:00:04 -0800

    commiting 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main  - Entry point
+ *  Return: Always (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar. but the outcome is a place of art,\n");
+	return (0);
+}
