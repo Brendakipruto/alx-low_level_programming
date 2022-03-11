c57985991a67cdb4901c8779d1e997221cd62769
Brendakipruto <brendakipruto16@gmail.com>
2022-03-10 21:48:06 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.>
+/**
+ * main - the variable n will store a diffrent value every time this program runs
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	if (n > 0)
+	{
+		printf(is negative)
+	}
+	return (0);
+}
