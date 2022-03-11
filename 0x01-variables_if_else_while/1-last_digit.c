25afd08afd210395ace8692830c19d6a65627682
Brendakipruto <brendakipruto16@gmail.com>
2022-03-10 22:14:29 -0800

    1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - 
+ *
+ * Return 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+	srandtime(0));
+	n = rand() - RAND_MAX / 2;
+        if (n > 5)
+		if (n = 0)
+		{
+			printf( abnd is greater than 5);
+		}
+	if ( n < 6)
+	{
+		printf(and is less than 6 not 0"\n");
+	return (0);
+}
